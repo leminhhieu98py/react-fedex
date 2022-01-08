6ebdd1d46a1c21d14165758dfd3c2f0aecb6bb5a
Le Minh Hieu <leminhhieu98py@gmail.com>
2022-01-08 10:14:57 +0700

    change default page

--- src/pages/Default/Default.jsx
+++ src/pages/Default/Default.jsx
@@ -5,13 +5,11 @@ import Grid from '@mui/material/Grid';
 
 const Default = () => {
     return (
-        <div className="content__wrapper">
-            <Grid container spacing={3}>
-                <Grid item xs={12} style={{textAlign: "center"}}>
-                    <img src={img} style={{width: "70%", marginTop: "80px"}} alt="Welcome back!" />
-                </Grid>
+        <Grid container spacing={3}>
+            <Grid item xs={12} style={{ textAlign: "center" }}>
+                <img src={img} style={{ width: "70%", marginTop: "80px" }} alt="Welcome back!" />
             </Grid>
-        </div>
+        </Grid>
     )
 }
 
